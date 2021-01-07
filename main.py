0bdcfeb78b08f4e7fa2c1f6a4ffea0c52f112692
Videsh Arivazhagan <arivvid27@outlook.com>
2021-01-07 00:06:29 +0000

    Made a face animation

--- main.py
+++ main.py
@@ -68,7 +68,30 @@ print('Please comment what you think about me and what arivvid27 should add to m
 
 sleep(4)
 print('SHUTTING DOWN...')
-print(''':) bye! You might see me next time with upgrades!
+
+print(':) bye! You might see me next time with more upgrades!')
+for i in range(10000000000000):
+	print('''
+ ______________________________
+[                              ]
+[__________           _________]
+[|___()___|         |__()_____|]
+[                              ]
+[       -____________-         ]
+[______________________________]
+''')
+for i in range(5000000000000):
+	print('''
+ ______________________________
+[                              ]
+[__________                    ]
+[|___()___|           _________]
+[                              ]
+[       -____________-         ]
+[______________________________]
+''')
+for i in range(10000000000000):
+	print('''
  ______________________________
 [                              ]
 [__________           _________]
